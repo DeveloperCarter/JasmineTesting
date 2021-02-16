098504dee81e0c757670845bf0e7be92611a3dc6
Carter <carter531@icloud.com>
2021-02-16 15:51:08 -0600

    Updated with new test files

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,17 +1,36 @@
 describe("Servers test (with setup and tear-down)", function() {
-  beforeEach(function () {
-    // initialization logic
-    serverNameInput.value = 'Alice';
-  });
+    beforeEach(function() {
+        // initialization logic
+        serverNameInput.value = 'Alice';
+    });
 
-  it('should add a new server to allServers on submitServerInfo()', function () {
-    submitServerInfo();
+    it('should add a new server to allServers on submitServerInfo()', function() {
+        submitServerInfo();
 
-    expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
-  });
+        expect(Object.keys(allServers).length).toEqual(1);
+        expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
 
-  afterEach(function() {
-    // teardown logic
-  });
+    it('should not add a new server on submitServerInfo() with empty input', function() {
+        serverNameInput.value = '';
+        submitServerInfo();
+
+        expect(Object.keys(allServers).length).toEqual(0);
+    });
+
+    it('should update #servertable on updateServerTable()', function() {
+        submitServerInfo();
+        updateServerTable();
+
+        let serverTable = document.querySelectorAll('#serverTable tbody tr td')
+        expect(serverTable.length).toEqual(3);
+        expect(serverTable[0].innerText).toEqual('Alice');
+        expect(serverTable[1].innerText).toEqual('$0.00');
+        expect(serverTable[2].innerText).toEqual('X');
+    });
+
+    afterEach(function() {
+        allServers = {};
+        serverId = 0;
+    });
 });
